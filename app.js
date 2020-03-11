cadaa8a6ebdf69edc87eadc9ff2675a3c16bd3dd
TL Chang <coooo77@hotmail.com>
2020-03-11 16:12:01 +0800

    Add Mongoose connection and create Record and User model

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
-
 const port = 3000 // = process.env
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/expense_tracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+const Record = require('./model/record')
+const User = require('./model/user')
 
 app.get('/', (req, res) => {
   res.send('hello world')
