80e590dcec0d98dff4ab6572147515882e36b637
TL Chang <coooo77@hotmail.com>
2020-03-13 17:50:24 +0800

    Add user login feature using passport

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const passport = require('passport')
 const app = express()
 const methodOverride = require('method-override')
 const session = require('express-session')
@@ -30,6 +31,15 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+
+app.use((req, res, next) => {
+  console.log('req.user', req.user)
+  res.locals.user = req.user
+  next()
+})
 
 app.use('/', require('./routes/home'))
 app.use('/users', require('./routes/user'))
