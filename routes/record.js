e33e7b871eabbf7ea5e18a04f28170c2b564f41e
TL Chang <coooo77@hotmail.com>
2020-03-14 21:54:48 +0800

    add check for adding or editing a record

--- routes/record.js
+++ routes/record.js
@@ -13,7 +13,6 @@ router.get('/', authenticated, (req, res) => {
   })
     .lean()
     .exec((err, records) => {
-      console.log('records', records)
       const totalAmount = moneyCalculation(records)
       if (err) return console.error(err)
       return res.render('index', { records, totalAmount })
@@ -28,14 +27,20 @@ router.get('/new', authenticated, (req, res) => {
 // 新增records
 router.post('/', authenticated, (req, res) => {
   const data = req.body
-  console.log('req.body', req.body)
+  const select = { [data.category]: true }
+  const check = Object.values(data).filter(value => value === "").length
   data.userId = req.user._id
-  const record = new Record(data)
-  record[req.body.category] = true
-  record.save(err => {
-    if (err) return console.log(err)
-    return res.redirect('/')
-  })
+  if (check) {
+    const errors = [{ message: '資料不齊全' }]
+    res.render('new', { data, errors, select })
+  } else {
+    const record = new Record(data)
+    record[req.body.category] = true
+    record.save(err => {
+      if (err) return console.log(err)
+      return res.redirect('/')
+    })
+  }
 })
 
 // 編輯records頁面
@@ -43,26 +48,86 @@ router.get('/:id/edit', authenticated, (req, res) => {
   Record.findOne({
     _id: req.params.id,
     userId: req.user._id,
-  }, (err, record) => {
-    if (err) return console.error(err)
-    return res.render('edit', record)
   })
+    .lean()
+    .exec((err, record) => {
+      if (err) return console.error(err)
+      return res.render('edit', record)
+    })
 })
 
+// 如果return res.render('edit', record)中的record沒有用{}，則可以用this作指派
+// 如果硬要寫{}，下面寫法要用.lean()處理，不然得到的不是純物件，會有警告
+// 什麼時候才要用.lean().exec? 看教材好像只有從資料庫讀取資料時才會用到
+// router.get('/:id/edit', authenticated, (req, res) => {
+//   Record.findOne({
+//     _id: req.params.id,
+//     userId: req.user._id,
+//   }, (err, record) => {
+//     console.log('record', record)
+//     console.log('record._id', record._id)
+//     if (err) return console.error(err)
+//     return res.render('edit', { record })
+//   })
+// }) 
+
 // 編輯records
+// 編輯的data check要怎麼寫比較好?一個欄位故意不填寫就會產生BUG，可是用兩種{{}}感覺很微妙，是否有更優雅的方法?
+// 看tim同學的作法，用input用required就好，不過可以故意修改頁面繞過這個問題的樣子?
+// 當編輯records的時候，用.lean().exec()處理的資料會沒辦法用.save .....?!
+// TypeError: record.save is not a function
+// 以下為會產生錯誤的寫法
+// router.put('/:id/edit', authenticated, (req, res) => {
+//   Record.findOne({
+//     _id: req.params.id,
+//     userId: req.user._id,
+//   })
+//     .lean()
+//     .exec((err, record) => {
+//       const data = req.body
+//       data._id = record._id
+//       const select = { [data.category]: true }
+//       const check = Object.values(data).filter(value => value === "").length
+//       if (check) {
+//         const errors = [{ message: '資料不齊全' }]
+//         console.log('data', data)
+//         res.render('edit', { data, errors, select })
+//       } else {
+//         if (err) return console.error(err)
+//         record[record.category] = false
+//         record[req.body.category] = true
+//         Object.assign(record, req.body)
+//         record.save(err => {
+//           if (err) return console.error(err)
+//           return res.redirect('/')
+//         })
+//       }
+//     })
+// })
+
 router.put('/:id/edit', authenticated, (req, res) => {
   Record.findOne({
     _id: req.params.id,
     userId: req.user._id,
   }, (err, record) => {
-    if (err) return console.error(err)
-    record[record.category] = false
-    record[req.body.category] = true
-    Object.assign(record, req.body)
-    record.save(err => {
+    const data = req.body
+    data._id = record._id
+    const select = { [data.category]: true }
+    const check = Object.values(data).filter(value => value === "").length
+    if (check) {
+      const errors = [{ message: '資料不齊全' }]
+      console.log('data', data)
+      res.render('edit', { data, errors, select })
+    } else {
       if (err) return console.error(err)
-      return res.redirect('/')
-    })
+      record[record.category] = false
+      record[req.body.category] = true
+      Object.assign(record, req.body)
+      record.save(err => {
+        if (err) return console.error(err)
+        return res.redirect('/')
+      })
+    }
   })
 })
 
